5c4519859a468431ecee576473e69340a14f9f96
rawinan.s <rawinan.soma@gmail.com>
2025-06-17 22:57:37 +0700

    Import Configmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,27 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { BookingsModule } from './bookings/bookings.module';
 import { PrismaModule } from 'prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [UsersModule, AuthModule, BookingsModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        JWT_ACCESS_TOKEN_SECRET: Joi.string().required(),
+        JWT_REFRESH_TOKEN_SECRET: Joi.string().required(),
+        JWT_ACCESS_TOKEN_EXP_TIME: Joi.number().required(),
+        JWT_REFRESH_TOKEN_EXP_TIME: Joi.number().required(),
+        SERVER_PORT: Joi.number().required(),
+        SERVER_URL: Joi.string().required(),
+        ENDPOINT_PREFIX: Joi.string().required(),
+      }),
+    }),
+    UsersModule,
+    AuthModule,
+    BookingsModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
